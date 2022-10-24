eb05d46d512248d517045084e12c994e41d255a2
ha-manel <manelhammouche3@gmail.com>
2022-10-24 20:47:39 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import { useRoutes } from 'react-router-dom';
+import routes from './config/routes';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
-  return <div></div>;
+  const appRoutes = useRoutes(routes);
+  return (
+    <div>
+      <header>
+        <Navbar />
+      </header>
+      <main className="w-full flex flex-col items-center">{appRoutes}</main>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/config/routes.js
@@ -0,0 +1,9 @@
+import Home from '../pages/home/Home';
+
+const routes = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+];
+export default routes;
