9f7da7f4b27a3c76a5c14c06d904766cce66113f
ha-manel <manelhammouche3@gmail.com>
2022-10-25 20:42:29 +0100

    Add why us section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Discount from '../../components/discount/Discount';
 import Hero from '../../components/hero/Hero';
 import NewProducts from '../../components/newProducts/NewProducts';
+import WhyUs from '../../components/whyUs/WhyUs';
 
 const Home = () => (
   <div className="w-full flex flex-col items-center">
     <Hero />
     <Discount />
     <NewProducts />
+    <WhyUs />
   </div>
 );
 
