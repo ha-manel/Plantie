3768f04b34269d9255410baf036d1f187c614a39
ha-manel <manelhammouche3@gmail.com>
2022-11-08 23:01:55 +0100

    Make trending responsive

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -5,7 +5,7 @@ import TrendingCard from './TrendingCard';
 const Trending = ({ products }) => (
   <section className="w-full flex flex-col items-center bg-white py-24">
     <h2 className="w-2/3 header">Trending Products</h2>
-    <div className="flex flex-wrap justify-center">
+    <div className="flex flex-wrap justify-center mt-10">
       {products.map((product) => (
         <TrendingCard key={product.name} product={product} />
       ))}
