f42163b3e09151d06fd406f4b90da855b54de7ec
ha-manel <manelhammouche3@gmail.com>
2022-11-08 22:48:57 +0100

    Add trending component

---
+++ src/components/trending/Trending.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TrendingCard from './TrendingCard';
+
+const Trending = ({ products }) => (
+  <section className="w-full flex flex-col items-center bg-white py-24">
+    <h2 className="w-2/3 header">Trending Products</h2>
+    <div className="flex flex-wrap justify-center">
+      {products.map((product) => (
+        <TrendingCard key={product.name} product={product} />
+      ))}
+    </div>
+  </section>
+);
+
+Trending.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default Trending;
