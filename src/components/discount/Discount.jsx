8b6b3b6ce05da7f97d643e1f8da47977a7d37c92
ha-manel <manelhammouche3@gmail.com>
2022-11-08 22:47:54 +0100

    Update bg color

--- src/components/discount/Discount.jsx
+++ src/components/discount/Discount.jsx
@@ -5,7 +5,7 @@ const Discount = () => (
     className="w-11/12 max-w-5xl sm:mx-0 sm:w-2/3 h-48 my-20 bg-no-repeat bg-center bg-cover rounded-xl overflow-hidden"
     style={{ backgroundImage: 'url("./assets/images/img3.png")' }}
   >
-    <div className="w-2/3 sm:w-1/2 h-full px-2 py-7 lg:px-16 lg:py-7 xl:px-20 xl:py-9 bg-black/50 rounded-tr-full">
+    <div className="w-2/3 sm:w-1/2 h-full px-2 py-7 lg:px-16 lg:py-7 xl:px-20 xl:py-9 bg-primary-500/50 rounded-tr-full">
       <div>
         <span className="block pl-1 mb-2 file:font-inter text-2xl font-bold text-lime-100">
           Special Offer:
