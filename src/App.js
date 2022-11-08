b16f50d580131ae7543d0185c2b25f68c80dc8ac
ha-manel <manelhammouche3@gmail.com>
2022-11-08 22:48:20 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useRoutes } from 'react-router-dom';
 import routes from './config/routes';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   const appRoutes = useRoutes(routes);
@@ -11,6 +12,7 @@ const App = () => {
         <Navbar />
       </header>
       <main className="w-full flex flex-col items-center">{appRoutes}</main>
+      <Footer />
     </div>
   );
 };
