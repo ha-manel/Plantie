64835ae88d633fca2b5dc09a2787971784d2927f
ha-manel <manelhammouche3@gmail.com>
2022-11-08 22:49:16 +0100

    Add shop route

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,9 +1,14 @@
 import Home from '../pages/home/Home';
+import Shop from '../pages/shop/Shop';
 
 const routes = [
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/shop',
+    element: <Shop />,
   },
 ];
 export default routes;
