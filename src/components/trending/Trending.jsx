6c6ba2b76bd9732669458978d836d4898e4d7d98
ha-manel <manelhammouche3@gmail.com>
2022-11-13 14:53:52 +0100

    Filter products

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TrendingCard from './TrendingCard';
 
-const Trending = ({ products }) => (
-  <section className="w-full flex flex-col items-center bg-white py-24">
-    <h2 className="w-2/3 header">Trending Products</h2>
-    <div className="flex flex-wrap justify-center mt-10">
-      {products.map((product) => (
-        <TrendingCard key={product.name} product={product} />
-      ))}
-    </div>
-  </section>
-);
+const Trending = ({ products }) => {
+  const trending = products.filter((plant) => plant.trending === true);
+  return (
+    <section className="w-full flex flex-col items-center bg-white py-24">
+      <h2 className="w-2/3 header">Trending Products</h2>
+      <div className="flex flex-wrap justify-center mt-10">
+        {trending.map((product) => (
+          <TrendingCard key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 Trending.propTypes = {
   products: PropTypes.arrayOf(
