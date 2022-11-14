28d18dbef2acb31fda9d9c459f8922aec317fa3c
ha-manel <manelhammouche3@gmail.com>
2022-11-14 18:10:37 +0100

    Update routes and react query

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
 import './App.css';
-import { createContext } from 'react';
-import { useRoutes } from 'react-router-dom';
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query';
-import routes from './config/routes';
+import { Route, Routes } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Shop from './pages/shop/Shop';
+import Contact from './pages/contact/Contact';
+import PlantDetails from './components/plantDetails/PlantDetails';
 
 const App = () => {
-  const appRoutes = useRoutes(routes);
-  const queryClient = new QueryClient();
-
   const fetchPlants = async () => {
     const response = await fetch('http://localhost:3000/api/v1/plants');
     return response.json();
   };
 
-  const query = useQuery('plants', fetchPlants);
-
-  const PlantsContext = createContext([]);
+  const { data, status } = useQuery({ queryKey: ['plants'], queryFn: fetchPlants });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div>
-        <header>
-          <Navbar />
-        </header>
-        <PlantsContext.Provider value={query}>
-          <main className="w-full flex flex-col items-center">{appRoutes}</main>
-        </PlantsContext.Provider>
-        <Footer />
-      </div>
-    </QueryClientProvider>
+    <div>
+      <header>
+        <Navbar />
+      </header>
+      <main className="w-full flex flex-col items-center">
+        <Routes>
+          <Route path="/" element={<Home data={data} status={status} />} />
+          <Route path="/shop" element={<Shop data={data} status={status} />} />
+          <Route path="/plant" element={<PlantDetails />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
