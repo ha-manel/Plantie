450cc30c4ee43ee144be180c4641e4d2b67b45c0
ha-manel <manelhammouche3@gmail.com>
2022-11-14 18:12:37 +0100

    Add data from API

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,52 +1,65 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Discount from '../../components/discount/Discount';
 import Hero from '../../components/hero/Hero';
-import Products from '../../components/products/Products';
+import ProductsCarousel from '../../components/productsCarousel/ProductsCarousel';
 import WhyUs from '../../components/whyUs/WhyUs';
 
-const Home = () => {
-  const products = [
-    {
-      id: 1,
-      name: 'Aloe Vera',
-      picture: './assets/images/plant1.webp',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam reprehenderit.',
-      price: 50.25,
-    },
-    {
-      id: 2,
-      name: 'Aloe Vera',
-      picture: './assets/images/plant2.webp',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam reprehenderit.',
-      price: 50.25,
-    },
-    {
-      id: 3,
-      name: 'Aloe Vera',
-      picture: './assets/images/plant3.webp',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam reprehenderit.',
-      price: 50.25,
-    },
-    {
-      id: 4,
-      name: 'Aloe Vera',
-      picture: './assets/images/plant4.webp',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam reprehenderit.',
-      price: 50.25,
-    },
-  ];
+const Home = ({ data, status }) => {
+  let plants = [];
+  if (status === 'success') {
+    plants = data.new;
+  }
   return (
     <div className="w-full flex flex-col items-center">
       <Hero />
       <Discount />
-      <Products title="New Products" products={products} />
+      <ProductsCarousel title="New Products" products={plants} />
       <WhyUs />
     </div>
   );
+};
+
+Home.propTypes = {
+  status: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    new: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        picture: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+        discount: PropTypes.number.isRequired,
+      }),
+    ),
+    discount: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        picture: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+        discount: PropTypes.number.isRequired,
+      }),
+    ),
+    indoor: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        picture: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+        discount: PropTypes.number.isRequired,
+      }),
+    ),
+    outdoor: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        picture: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+        discount: PropTypes.number.isRequired,
+      }),
+    ),
+  }).isRequired,
 };
 
 export default Home;
