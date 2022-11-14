4b088ad64537067c0c02c2ec5850a42d664fffc2
ha-manel <manelhammouche3@gmail.com>
2022-11-14 18:13:21 +0100

    Change cart icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,7 +7,11 @@ const Navbar = () => {
     <div className="w-screen px-10 pt-2 pb-4 bg-secondary-200">
       <nav className="flex justify-between items-center">
         <div>
-          <img src="./assets/images/logo.png" alt="logo" className="hidden sm:block w-40" />
+          <img
+            src="./assets/images/logo.png"
+            alt="logo"
+            className="hidden sm:block w-40"
+          />
         </div>
         <ul className="flex pt-2 font-inter font-medium text-lg">
           <li className="mx-4">
@@ -64,7 +68,7 @@ const Navbar = () => {
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
                 />
               </svg>
             </NavLink>
