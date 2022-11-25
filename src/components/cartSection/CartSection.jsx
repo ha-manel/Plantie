04b0344b5e4b18e2ff82c87a19290742f9bf8c9f
ha-manel <manelhammouche3@gmail.com>
2022-11-25 19:44:45 +0100

    Fix reduce function

--- src/components/cartSection/CartSection.jsx
+++ src/components/cartSection/CartSection.jsx
@@ -9,7 +9,8 @@ const CartSection = () => {
   const calculateTotal = (items) => {
     if (items.length > 1) {
       const total = items.reduce(
-        (x, y) => x.product.discount * x.quantity + y.product.discount * y.quantity,
+        (sum, currentItem) => sum + currentItem.product.discount * currentItem.quantity,
+        0,
       );
       setTotalPrice(total);
     } else if (items.length === 1) {
@@ -25,7 +26,25 @@ const CartSection = () => {
   }, []);
 
   if (items.length < 1) {
-    return <div>You don&apos;t have any items yet</div>;
+    return (
+      <div className="flex items-center mt-24 text-2xl font-inter font-bold text-gray-600">
+        You don&apos;t have any items yet
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-8 h-8 ml-2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+          />
+        </svg>
+      </div>
+    );
   }
 
   return (
