d2841128d4c2f38871d59ebf9c54a0086ae0db2f
ha-manel <manelhammouche3@gmail.com>
2022-11-25 19:46:09 +0100

    Update routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,6 +1,10 @@
+import PlantDetails from '../components/plantDetails/PlantDetails';
+import Cart from '../pages/cart/Cart';
 import Contact from '../pages/contact/Contact';
 import Home from '../pages/home/Home';
+import Profile from '../pages/profile/Profile';
 import Shop from '../pages/shop/Shop';
+import Signup from '../pages/signup/Signup';
 
 const routes = [
   {
@@ -14,6 +18,22 @@ const routes = [
   {
     path: '/contact',
     element: <Contact />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/plant',
+    element: <PlantDetails />,
   },
 ];
 export default routes;
