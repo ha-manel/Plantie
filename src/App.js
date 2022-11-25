a24a277bd70062cc4b90a912e4de075d0cfbfa02
ha-manel <manelhammouche3@gmail.com>
2022-11-25 19:44:23 +0100

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,80 +1,64 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import Home from './pages/home/Home';
-import Shop from './pages/shop/Shop';
-import Contact from './pages/contact/Contact';
-import PlantDetails from './components/plantDetails/PlantDetails';
-import Cart from './pages/cart/Cart';
-import Profile from './pages/profile/Profile';
-import Signup from './pages/signup/Signup';
+import UserContext from './contexts/userContext';
+import PlantsContext from './contexts/plantsContext';
+import WishlistContext from './contexts/wishlistContext';
+import Layout from './config/layout/Layout';
 
 const App = () => {
+  const [wishlist, setWishlist] = useState([]);
   const [user, setUser] = useState({
     isLoggedIn: false,
     user: {},
   });
 
-  const handleLogin = (data) => {
-    setUser({
-      isLoggedIn: true,
-      user: data.user,
+  const loginStatus = async () => {
+    const response = await axios.get('http://localhost:3000/api/v1/logged_in', {
+      withCredentials: true,
     });
-  };
-
-  const handleLogout = () => {
-    setUser({
-      isLoggedIn: false,
-      user: {},
-    });
-  };
-
-  const loginStatus = () => {
-    axios
-      .get('http://localhost:3000/api/v1/logged_in', { withCredentials: true })
-      .then((response) => {
-        if (response.data.logged_in) {
-          console.log(response.data);
-          handleLogin(response.data);
-        } else {
-          handleLogout();
-        }
-      })
-      .catch((error) => console.log('api errors:', error));
-  };
-
-  useEffect(() => {
-    loginStatus();
-  }, []);
-
-  const fetchPlants = async () => {
-    const response = await axios.get('http://localhost:3000/api/v1/plants', { withCredentials: true });
     return response.data;
   };
 
-  const { data, status } = useQuery({ queryKey: ['plants'], queryFn: fetchPlants });
+  useQuery({
+    queryKey: ['loginStatus'],
+    queryFn: loginStatus,
+    onSuccess: (data) => {
+      if (data.logged_in) {
+        setUser({
+          isLoggedIn: true,
+          user: data.user,
+        });
+        setWishlist(data.wishlist);
+      }
+    },
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
+
+  const fetchPlants = async () => {
+    const response = await axios.get('http://localhost:3000/api/v1/plants', {
+      withCredentials: true,
+    });
+    return response.data;
+  };
+
+  const plantsData = useQuery({
+    queryKey: ['plants'],
+    queryFn: fetchPlants,
+    refetchOnWindowFocus: false,
+  });
 
   return (
     <>
-      <header>
-        <Navbar user={user} handleLogout={handleLogout} />
-      </header>
-      <main className="w-full flex flex-col items-center pb-36">
-        <Routes>
-          <Route path="/" element={<Home data={data} status={status} />} />
-          <Route path="/shop" element={<Shop data={data} status={status} />} />
-          <Route path="/plant" element={<PlantDetails />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/profile" element={<Profile user={user} handleLogin={handleLogin} />} />
-          <Route path="/signup" element={<Signup handleLogin={handleLogin} user={user} />} />
-        </Routes>
-      </main>
-      <Footer />
+      <UserContext.Provider value={[user, setUser]}>
+        <PlantsContext.Provider value={plantsData}>
+          <WishlistContext.Provider value={[wishlist, setWishlist]}>
+            <Layout />
+          </WishlistContext.Provider>
+        </PlantsContext.Provider>
+      </UserContext.Provider>
     </>
   );
 };
