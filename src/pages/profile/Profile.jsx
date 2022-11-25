8a6bf2bc4cf66d5446eee81a32eca18348e2d168
ha-manel <manelhammouche3@gmail.com>
2022-11-25 23:52:37 +0100

    Extract component

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,109 +1,17 @@
-/* eslint-disable react/prop-types */
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import Wishlist from '../../components/wishlist/Wishlist';
+import UserContext from '../../contexts/userContext';
 
-const Profile = ({ user, handleLogin }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const Profile = () => {
+  const [user] = useContext(UserContext);
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const user = {
-      username,
-      password,
-    };
-    axios
-      .post(
-        'http://localhost:3000/api/v1/login',
-        { user },
-        { withCredentials: true },
-      )
-      .then((response) => {
-        if (response.data.logged_in) {
-          handleLogin(response.data);
-        } else {
-          console.log('errors', response.data.errors);
-        }
-      })
-      .catch((error) => console.log('api errors:', error));
-  };
-
-  if (user.isLoggedIn) {
-    return (
-      <div>
-        Hello
-        {user.user.username}
-      </div>
-    );
+  if (!user.isLoggedIn) {
+    return <Navigate to="/signin" replace />;
   }
 
   return (
-    <div className="w-full flex justify-center mt-7">
-      <div className="w-3/5 flex flex-col items-center py-10 px-24">
-        <h1 className="header">Log In</h1>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col items-center w-3/5 mt-6"
-        >
-          <div className="form-control w-full max-w-xs font-inter">
-            <label className="label text-md" htmlFor="name">
-              <span className="label-text">Username:</span>
-            </label>
-            <input
-              type="text"
-              id="name"
-              placeholder="John"
-              value={username}
-              onChange={updateUsername}
-              className="input rounded-md border border-secondary-200 outline-primary-200 w-full max-w-xs mt-1"
-            />
-          </div>
-          <div className="form-control w-full max-w-xs mt-3 font-inter">
-            <label className="label text-md" htmlFor="password">
-              <span className="label-text">Password:</span>
-            </label>
-            <input
-              type="text"
-              id="password"
-              placeholder="****"
-              value={password}
-              onChange={updatePassword}
-              className="input rounded-md border border-secondary-200 outline-primary-200 w-full max-w-xs mt-1"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-2/3 mt-6 py-3 transition ease-out duration-300 rounded-lg bg-primary-300 hover:bg-primary-200 text-white font-semibold text-md font-inter"
-          >
-            Log In
-          </button>
-          <div className="font-inter text-md font-semibold mt-6">
-            or
-            <Link
-              to="/signup"
-              className="ml-2 underline underline-offset-2 hover:text-primary-300 transition ease-out duration-300"
-            >
-              sign up
-            </Link>
-          </div>
-        </form>
-      </div>
-      <div className="hidden md:flex w-2/5 justify-center bg-secondary-100 rounded-l-full">
-        <img
-          src="./assets/images/login-img.png"
-          alt="login"
-          className="w-3/5 object-cover"
-        />
-      </div>
-    </div>
+    <Wishlist />
   );
 };
 
